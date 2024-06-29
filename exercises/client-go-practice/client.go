e650511d405fd4aff279e08e3574f3c81b7c6c7a
Gr1nx <gr1nx614@gmail.com>
2024-06-29 12:23:44 -0700

    some more advanced stuff! got to copy a pod and then wrap a deployment around it :)

--- exercises/client-go-practice/client.go
+++ exercises/client-go-practice/client.go
@@ -27,20 +27,10 @@ func main() {
 		panic(err)
 	}
 
-	// namespace := &apiv1.Namespace{
-	// 	ObjectMeta: metav1.ObjectMeta{
-	// 		Name: "blue",
-	// 	},
-	// }
-
-	// ns, err := clientset.CoreV1().Namespaces().Create(context.TODO(), namespace, metav1.CreateOptions{})
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Printf("Namespace %q created!\n", ns.GetObjectMeta().GetName())
-	podSpec := apiv1.PodTemplateSpec{
+	myPod := &apiv1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
+			Name:      "custom-pod",
+			Namespace: "default",
 			Labels: map[string]string{
 				"app": "test",
 			},
@@ -48,23 +38,60 @@ func main() {
 		Spec: apiv1.PodSpec{
 			Containers: []apiv1.Container{
 				{
-					Name:  "deployment-pod",
+					Name:  "custom-pod-container",
 					Image: "nginx",
 				},
 			},
 		},
 	}
+
+	testPod, err := clientset.CoreV1().Pods("default").Create(context.TODO(), myPod, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Sprintf("Successfully create %q pod!", testPod.Name)
+
+	// podSpec := apiv1.PodTemplateSpec{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Labels: map[string]string{
+	// 			"app": "test",
+	// 		},
+	// 	},
+	// 	Spec: apiv1.PodSpec{
+	// 		Containers: []apiv1.Container{
+	// 			{
+	// 				Name:  "deployment-pod",
+	// 				Image: "nginx",
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "custom-pod", metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	podSpec := apiv1.PodTemplateSpec{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   pod.Name,
+			Labels: pod.Labels,
+		},
+		Spec: pod.Spec,
+	}
+
 	result, err := createDeployment("test-deployment", "blue", 3, podSpec, clientset)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Deployment %q created!\n", result.GetObjectMeta().GetName())
 
-	if err = deleteDeployment("test-deployment", "blue", clientset); err != nil {
-		panic(err)
-	}
+	// if err = deleteDeployment("test-deployment", "blue", clientset); err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println("Deployment deleted!")
+	// fmt.Println("Deployment deleted!")
 
 	// pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "test", metav1.GetOptions{})
 	// if err != nil {
@@ -104,4 +131,15 @@ func createDeployment(name string, ns string, replicas int, podSpec apiv1.PodTem
 func deleteDeployment(name string, ns string, client *kubernetes.Clientset) error {
 	err := client.AppsV1().Deployments(ns).Delete(context.TODO(), name, *metav1.NewDeleteOptions(int64(5)))
 	return err
+}
+
+func createNamespace(name string, client *kubernetes.Clientset) (*apiv1.Namespace, error) {
+	namespace := &apiv1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+
+	ns, err := client.CoreV1().Namespaces().Create(context.TODO(), namespace, metav1.CreateOptions{})
+	return ns, err
 }
