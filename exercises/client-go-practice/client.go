8e8f560a1c4611031ad6900e4f76d43e6d36fb32
Gr1nx <gr1nx614@gmail.com>
2024-06-29 11:58:40 -0700

    made some really good progress! working on seperating out functions for the practice next; I'll have to continue reading on the controller stuff though

--- exercises/client-go-practice/client.go
+++ exercises/client-go-practice/client.go
@@ -10,6 +10,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	pointer "k8s.io/utils/pointer"
 )
 
 func main() {
@@ -26,50 +27,81 @@ func main() {
 		panic(err)
 	}
 
-	var replicas *int32
-	*replicas = 2
+	// namespace := &apiv1.Namespace{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name: "blue",
+	// 	},
+	// }
+
+	// ns, err := clientset.CoreV1().Namespaces().Create(context.TODO(), namespace, metav1.CreateOptions{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Namespace %q created!\n", ns.GetObjectMeta().GetName())
+	podSpec := apiv1.PodTemplateSpec{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{
+				"app": "test",
+			},
+		},
+		Spec: apiv1.PodSpec{
+			Containers: []apiv1.Container{
+				{
+					Name:  "deployment-pod",
+					Image: "nginx",
+				},
+			},
+		},
+	}
+	result, err := createDeployment("test-deployment", "blue", 3, podSpec, clientset)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Deployment %q created!\n", result.GetObjectMeta().GetName())
+
+	if err = deleteDeployment("test-deployment", "blue", clientset); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Deployment deleted!")
+
+	// pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "test", metav1.GetOptions{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(pod.Name)
+	// I wanna try creating resources at some point; we'll get there at some point
+}
+
+// currently this doesn't do any error checking i.e. it does not check if a deployment under the same name already exists
+// or if the namespace is valid although I don't think the second problem actually matters with the right cluster parameters
+func createDeployment(name string, ns string, replicas int, podSpec apiv1.PodTemplateSpec, client *kubernetes.Clientset) (*v1.Deployment, error) {
+	reps := pointer.Int32(int32(replicas))
 
 	deployment := &v1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-deployment",
+			Name: name,
 		},
 		Spec: v1.DeploymentSpec{
-			Replicas: replicas,
+			Replicas: reps,
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
 					"app": "test",
 				},
 			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "test",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "deployment-pod",
-							Image: "nginx",
-						},
-					},
-				},
-			},
+			Template: podSpec,
 		},
 	}
 
-	result, err := clientset.AppsV1().Deployments("default").Create(context.TODO(), deployment, metav1.CreateOptions{})
-	if err != nil {
-		panic(err)
-	}
+	result, err := client.AppsV1().Deployments(ns).Create(context.TODO(), deployment, metav1.CreateOptions{})
 
-	fmt.Printf("Deployment %q created!\n", result.GetObjectMeta().GetName())
+	return result, err
+}
 
-	pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "test", metav1.GetOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(pod.Name)
-	// I wanna try creating resources at some point; we'll get there at some point
+// currently, this function does not do any error checking or handling
+func deleteDeployment(name string, ns string, client *kubernetes.Clientset) error {
+	err := client.AppsV1().Deployments(ns).Delete(context.TODO(), name, *metav1.NewDeleteOptions(int64(5)))
+	return err
 }
--- go.mod
+++ go.mod
@@ -38,7 +38,7 @@ require (
 	k8s.io/client-go v0.30.2 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
-	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+	k8s.io/utils v0.0.0-20240502163921-fe8a2dddb1d0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
