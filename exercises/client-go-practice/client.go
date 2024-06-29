d7c0cb02f442ce2557d4606726d4a018d73be802
Gr1nx <gr1nx614@gmail.com>
2024-06-29 01:04:39 -0700

    ehh, no problems with compliation for creating a deployment but it not working yet :(

--- exercises/client-go-practice/client.go
+++ exercises/client-go-practice/client.go
@@ -5,13 +5,15 @@ import (
 	"flag"
 	"fmt"
 
+	v1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "../config/config", "kubeconfig file")
+	kubeconfig := flag.String("kubeconfig", "../../config/config", "kubeconfig file")
 	flag.Parse()
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -23,6 +25,45 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	var replicas *int32
+	*replicas = 2
+
+	deployment := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-deployment",
+		},
+		Spec: v1.DeploymentSpec{
+			Replicas: replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "test",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "test",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "deployment-pod",
+							Image: "nginx",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	result, err := clientset.AppsV1().Deployments("default").Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Deployment %q created!\n", result.GetObjectMeta().GetName())
 
 	pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "test", metav1.GetOptions{})
 	if err != nil {
